dbcc01c4aa9f79e3fad7d5272fc8beff5d19bde3
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-25 13:20:03 -0500

    commit tree 291c696ebeb715fe9f5034d538d17f6579bc3033

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(arryOfSomething, stringOfSomething) {
+    const results = arryOfSomething.filter(arrayNames => arrayNames.toLowerCase() === stringOfSomething.toLowerCase());
+    return results;
+}
+
+function fuzzyMatch(arryOfSomething, stringOfSomething) {
+    const results = arryOfSomething.filter(arrayLetter => arrayLetter.charAt(0) === stringOfSomething.charAt(0));
+    return results;
+}
+
+function matchName(arryOfSomething, stringOfSomething) {
+    const results = arryOfSomething.filter(stringCompare => stringCompare.name === stringOfSomething)
+    return results
+}
